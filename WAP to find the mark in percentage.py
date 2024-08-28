deca386407a95a425db8eb5f14fb5e42ba7d35de
Joel Williams Mathew <joelw2200@gmail.com>
2024-08-28 17:49:20 +0530

    Add files via upload

---
+++ WAP to find the mark in percentage.py
@@ -0,0 +1,10 @@
+input("Enter Your Name :")
+M=float(input("Mark obtained in Maths :"))
+E=float(input("Mark obtained in English :"))
+CS=float(input("Mark obtaines in Computer Science :"))
+C=float(input("Mark obtained in Chemistry :"))
+P=float(input("Mark obtained in Physics :"))
+total=M+E+C+Ch+P
+per=(total/500)*100
+print("Total Mark obtained =",total,"/ 500.0")
+print("Total Percentage=",per, "%")
